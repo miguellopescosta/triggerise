a4600896e6919d94ed66a0babe5b2b393f4ce273
Miguel Costa <miguellopescosta@sapo.pt>
2021-11-02 08:10:07 +0000

    buyItem() test corrected

--- web-api/src/test/java/com/trigger/rest/StoreRestApiTest.java
+++ web-api/src/test/java/com/trigger/rest/StoreRestApiTest.java
@@ -1,32 +1,26 @@
 package com.trigger.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trigger.ConfigC;
+import com.trigger.Operations.CheckoutOperation;
 import com.trigger.StoreEngine;
-import com.trigger.parameters.CheckoutParameters;
 import com.trigger.rest.views.request.CheckoutRequest;
-import com.trigger.results.CheckoutResult;
-import com.trigger.results.Status;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.context.ApplicationContext;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -36,22 +30,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class StoreRestApiTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @Autowired
+    private ConfigC configC;
+
+    @Autowired
     private ModelMapper modelmapper;
 
-    @MockBean
+    @Autowired
     private StoreEngine storeEngine;
+
+    @Autowired
+    private CheckoutOperation checkoutOperation;
 
     @Test
     void obtainPricingList() throws Exception {
         RequestBuilder request = MockMvcRequestBuilders.get("/pricing");
         /* This integrated test verifies if the response is 200, further validations might be added */
-        MockHttpServletResponse response = mvc.perform(request).andExpect(status().isOk()).andReturn().getResponse();
+        MockHttpServletResponse response = mockMvc.perform(request).andExpect(status().isOk()).andReturn().getResponse();
         System.out.println("Result: " + response.getContentAsString());
     }
 
@@ -59,27 +62,12 @@ class StoreRestApiTest {
     void buyItems() throws Exception {
 
         CheckoutRequest requestExample = new CheckoutRequest(List.of("MUG","MUG","TSHIRT","USBKEY"));
-        CheckoutParameters params = new ModelMapper().map(requestExample, CheckoutParameters.class);
-        CheckoutResult checkoutResult = new CheckoutResult(Arrays.asList("USBKEY (1 items)", "TSHIRT (1 items)", "MUG (2 items)"),
-                new BigDecimal(35), new Status(new StringBuilder("Success"), new StringBuilder("Ok")));
 
-        /* For this exercise, we assume pricing rules have already been set, so we build one */
-        Map<String, Double> pricingRules = new HashMap<>();
-        pricingRules.put("MUG", 4.00);
-        pricingRules.put("TSHIRT", 21.00);
-        pricingRules.put("USBKEY", 10.00);
-
-        params.setPricingRules(pricingRules);
-
-        when(storeEngine.checkout(params)).thenReturn(checkoutResult);
-
-        RequestBuilder request = MockMvcRequestBuilders
-                .post("/checkout")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(requestExample))
-                .accept(MediaType.APPLICATION_JSON);
-        /* This integrated test verifies if the response is 200, further validations might be added */
-        this.mvc.perform(request).andDo(print()).andExpect(status().isOk());
+        mockMvc.perform(post("/checkout")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(requestExample)))
+                .andDo(print())
+                .andExpect(status().isOk());
 
     }
 
